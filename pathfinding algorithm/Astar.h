95de8e6accf299f9be959fc6d177780451e0bb6e
CaffineCicada <rmit722@aucklanduni.ac.nz>
2024-10-01 21:08:46 +1300

    dd

--- pathfinding algorithm/Astar.h
+++
@@ -1,9 +0,0 @@
-#ifndef ASTAR_H
-#define ASTAR_H
-
-#include <stdbool.h>
-#include "Astar.c"
-
-void astar(unsigned char map[15][19], unsigned char start_x, unsigned char start_y, unsigned char dest_x, unsigned char dest_y, unsigned char path[15][19], int *shortest_length);
-
-#endif 
