c0fbcbf0da73ba8cd1cec2826e9d01a5f9a1001d
CaffineCicada <rmit722@aucklanduni.ac.nz>
2024-10-06 02:45:39 +1300

    maybe try this??

--- CS301_Class.cydsn/controlMotor.c
+++ CS301_Class.cydsn/controlMotor.c
@@ -8,7 +8,7 @@
  * WHICH IS THE PROPERTY OF your company.
  *
  * ========================================
-*/
+ */
 #include <project.h>
 #include <distance.h>
 #include "controlMotor.h"
@@ -21,25 +21,23 @@ float percentage1, percentage2 = 0.00;
 
 uint8_t leftTurnCount = 0;
 
-
-
 // to be passed in from the pathfinder algorithm
 //--------------------------------------------
 // 0 = North, 1 = East, 2 = South, 3 = West
 uint8_t pathInstructions[] = {0, 0, 1, 2, 3, 3, 0, 3, 2, 3, 0, 3, 2};
 uint8_t turnsUntilNextNode[] = {13};
 uint8_t remainingDistance[] = {0};
-uint8_t j = 0;
-uint8_t i = 1;
+uint8_t i, j = 0;
+uint8_t location = 1;
 uint8_t direction;
 uint8_t lengthOfArray = sizeof(pathInstructions) / sizeof(pathInstructions[0]);
 //--------------------------------------------
 
-void motorControl() {
-    
-    
-    
-    if(stop == 1){
+void motorControl()
+{
+
+    if (stop == 1)
+    {
         PWMLeftMotor = 127;
         PWMRightMotor = 127;
         PWM_1_WriteCompare(PWMLeftMotor);
@@ -52,98 +50,97 @@ void motorControl() {
         PWM_2_WriteCompare(127);
     }
     */
-    else if (Q3 == 0 || Q4 == 0) {
-    
-        
+    else if (Q3 == 0 || Q4 == 0)
+    {
+
         // look into this
-        if (i == turnsUntilNextNode[j]){
+        if (i == turnsUntilNextNode[j])
+        {
             // we have reached the end of the node
             // implement travelling the remaining distance here
-            // j++;
+            j++;
+            i = 0;
         }
-        
-        //else {
-        
-            // apologies for this nasty code
-            // this is to determine the nature of the turn
-            
+
+        // else {
+
+        // apologies for this nasty code
+        // this is to determine the nature of the turn
+
+        // go straight
+        if ((pathInstructions[location] == 0 && pathInstructions[location - 1] == 0) || (pathInstructions[location] == 1 && pathInstructions[location - 1] == 1) || (pathInstructions[location] == 2 && pathInstructions[location - 1] == 2) || (pathInstructions[location] == 3 && pathInstructions[location - 1] == 3))
+        {
+
+            direction = 0;
+        }
+        // go left
+        else if ((pathInstructions[location] == 0 && pathInstructions[location - 1] == 1) || (pathInstructions[location] == 1 && pathInstructions[location - 1] == 3) || (pathInstructions[location] == 2 && pathInstructions[location - 1] == 3) || (pathInstructions[location] == 3 && pathInstructions[location - 1] == 0))
+        {
+
+            direction = 1;
+        }
+
+        // go right
+        else if ((pathInstructions[location] == 0 && pathInstructions[location - 1] == 3) || (pathInstructions[location] == 1 && pathInstructions[location - 1] == 0) || (pathInstructions[location] == 2 && pathInstructions[location - 1] == 1) || (pathInstructions[location] == 3 && pathInstructions[location - 1] == 2))
+        {
+
+            direction = 2;
+        }
+
+        // go back
+        else if ((pathInstructions[location] == 0 && pathInstructions[location - 1] == 2) || (pathInstructions[location] == 1 && pathInstructions[location - 1] == 2) || (pathInstructions[location] == 2 && pathInstructions[location - 1] == 0) || (pathInstructions[location] == 3 && pathInstructions[location - 1] == 1))
+        {
+
+            direction = 3;
+        }
+
+        // physical turning
+        switch (direction)
+        {
+        case 0:
             // go straight
-            if ((pathInstructions[i] == 0 && pathInstructions[i-1] == 0) || (pathInstructions[i] == 1 && pathInstructions[i-1] == 1) || (pathInstructions[i] == 2 && pathInstructions[i-1] == 2) || (pathInstructions[i] == 3 && pathInstructions[i-1] == 3)){
-            
-                direction = 0;
-            
-            }
-            // go left
-            else if((pathInstructions[i] == 0 && pathInstructions[i-1] == 1) || (pathInstructions[i] == 1 && pathInstructions[i-1] == 3) || (pathInstructions[i] == 2 && pathInstructions[i-1] == 3) || (pathInstructions[i] == 3 && pathInstructions[i-1] == 0)){
-            
-                direction = 1;
-            
-            }
+            PWM_1_WriteCompare(157);
+            PWM_2_WriteCompare(97);
+            break;
 
-            // go right
-            else if((pathInstructions[i] == 0 && pathInstructions[i-1] == 3) || (pathInstructions[i] == 1 && pathInstructions[i-1] == 0) || (pathInstructions[i] == 2 && pathInstructions[i-1] == 1) || (pathInstructions[i] == 3 && pathInstructions[i-1] == 2)){
-            
-                direction = 2;
-            
-            }
+        case 1:
+            // turn left
+            PWMLeftMotor = 90;
+            PWMRightMotor = 90;
+            PWM_1_WriteCompare(PWMLeftMotor);
+            PWM_2_WriteCompare(PWMRightMotor);
+            break;
 
-            // go back
-            else if((pathInstructions[i] == 0 && pathInstructions[i-1] == 2) || (pathInstructions[i] == 1 && pathInstructions[i-1] == 2) || (pathInstructions[i] == 2 && pathInstructions[i-1] == 0) || (pathInstructions[i] == 3 && pathInstructions[i-1] == 1)){
-            
-                direction = 3;
-            
-            }
+        case 2:
 
-            // physical turning
-            switch (direction)
-            {
-            case 0:
-                // go straight
-                PWM_1_WriteCompare(157);
-                PWM_2_WriteCompare(97);
-                break;
-            
-            /*case 1:
-                // turn left
-                PWMLeftMotor = 90;
-                PWMRightMotor = 90;
-                PWM_1_WriteCompare(PWMLeftMotor);
-                PWM_2_WriteCompare(PWMRightMotor);
-                break;
+            // turn right
+            PWMLeftMotor = 157;
+            PWMRightMotor = 103;
+            PWM_1_WriteCompare(PWMLeftMotor);
+            PWM_2_WriteCompare(PWMRightMotor);
+            break;
 
-            case 2:
-                
-                // turn right
-                PWMLeftMotor = 157;
-                PWMRightMotor = 103;
-                PWM_1_WriteCompare(PWMLeftMotor);
-                PWM_2_WriteCompare(PWMRightMotor); 
-                break;
+        case 3:
+            // turn around
+            PWMLeftMotor = 90;
+            PWMRightMotor = 90;
+            PWM_1_WriteCompare(PWMLeftMotor);
+            PWM_2_WriteCompare(PWMRightMotor);
+            break;
 
-            case 3:
-                // turn around
-                PWMLeftMotor = 90;
-                PWMRightMotor = 90;
-                PWM_1_WriteCompare(PWMLeftMotor);
-                PWM_2_WriteCompare(PWMRightMotor);
-                break;
-            */
-            default:
-                // stop
-                PWM_1_WriteCompare(127);
-                PWM_2_WriteCompare(127);
-                break;
-            }
-        
+        default:
+            // stop
+            PWM_1_WriteCompare(127);
+            PWM_2_WriteCompare(127);
+            break;
+        }
+
+        location++;
         i++;
-
-        
-        
     }
-    
-    
+
     /*
-    //for later use probably      
+    //for later use probably
     // light turn
     else if(Q4 == 0) {
         PWMLeftMotor = 160;
@@ -151,59 +148,60 @@ void motorControl() {
         PWM_1_WriteCompare(PWMLeftMotor);
         PWM_2_WriteCompare(PWMRightMotor);
     }
-    
+
     // left turn
     else if(Q3 == 0) {
         PWMLeftMotor = 90;
         PWMRightMotor = 90;
         PWM_1_WriteCompare(PWMLeftMotor);
         PWM_2_WriteCompare(PWMRightMotor);
-       
+
     }
     */
-    
+
     // save
     //----------------------------------------------
     // adjust to the right
-    else if(Q5 == 1 && Q6 == 0) {
+    else if (Q5 == 1 && Q6 == 0)
+    {
         PWMLeftMotor = 157;
         PWMRightMotor = 103;
         PWM_1_WriteCompare(PWMLeftMotor);
         PWM_2_WriteCompare(PWMRightMotor);
     }
-    
+
     // adjust to the left
-    else if(Q5 == 0 && Q6 == 1) {
+    else if (Q5 == 0 && Q6 == 1)
+    {
         PWMLeftMotor = 152;
         PWMRightMotor = 90;
         PWM_1_WriteCompare(PWMLeftMotor);
         PWM_2_WriteCompare(PWMRightMotor);
     }
     //----------------------------------------------
-    
+
     // ignore (this works)
     //---------------------------------------------------
     // Straight line
-    else if(Q5 == 1 && Q6 == 1) {
+    else if (Q5 == 1 && Q6 == 1)
+    {
         /*
-        //calculates the percentage the robot travels at and feeds to the PWM 
-        
+        //calculates the percentage the robot travels at and feeds to the PWM
+
         percentage1 = (((percentageOfSpeed - 40)*32)/60); //calculates percentage1
         percentage2 = (((percentageOfSpeed - 40)*34)/60); //calculates percentage2
-        
+
         //feeds the PWM with the updated values
         //minimum speed value = 155, 100
         //maximum speed value = 187, 66
         PWMLeftMotor = 155 + (uint8_t)percentage1;
         PWMRightMotor = 100 - (uint8_t)percentage2;
         */
-        //write to the motor
+        // write to the motor
         PWM_1_WriteCompare(157);
         PWM_2_WriteCompare(97);
     }
     //--------------------------------------------------
-    
-    
 }
 
 /* [] END OF FILE */
